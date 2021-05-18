53a2f053bab77678e3003f9c0a95a7c878e4e86a
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-05-18 15:35:25 -0400

    Initial commit

---
+++ src/main/java/com/tts/oauth2Demo/Oauth2DemoApplication.java
@@ -0,0 +1,54 @@
+package com.tts.oauth2Demo;
+
+// import statements
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+
+@SpringBootApplication
+@RestController
+public class Oauth2DemoApplication extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests(a -> a
+						.antMatchers("/", "/error").permitAll()
+						.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+				.oauth2Login().defaultSuccessUrl("/", true)
+				.and()
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/").deleteCookies("JSESSIONID")
+				.invalidateHttpSession(true);
+
+
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2DemoApplication.class, args);
+	}
+
+}
+
